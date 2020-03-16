fcc028250d0d5e8359794d7d29f0c12c4ea90f15
unknown <zxdddf@gmail.com>
2020-03-16 10:21:09 -0700

    copy paste

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,7 +9,30 @@ export class AuthComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  public showRegister:boolean = false;
+  public showLogin:boolean =true;
+  public buttonName:any = 'Show';
 
+  ngOnInit () {  }
+
+  toggleRegister() {
+    this.showRegister = !this.showRegister;
+    this.toggleLogin();
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.showRegister)  
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
+  }
+  toggleLogin(){
+    this.showLogin = !this.showLogin;
+    this.toggleRegister();
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if(this.showLogin)  
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
+  }
 }
